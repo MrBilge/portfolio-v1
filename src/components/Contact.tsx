0a65c7093c78cb41523234f5ad54346f6ddd4498
MrBilge <b.eybek9307@gmail.com>
2025-08-04 16:05:41 +0300

    fff

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
         <div className="lg:w-1/2">
           <div className="hidden lg:block border-t-4 gap-10 border-t-white p-4 w-24 lg:w-48"></div>
           <div className="h-full w-full space-y-10 ">
-            <h1 className="w-full lg:text-3xl xl:text-5xl tracking-widest text-center lg:text-start ">
+            <h1 className="w-full lg:text-3xl xl:text-5xl tracking-widest text-center lg:text-start">
               Interested in working together ? Let’s talk
               <ArrowRightIcon className="hidden lg:inline  w-20 h-20 text-blue-700  ml-3" />
               <ArrowDownIcon className=" inline lg:hidden w-10 h-10 text-blue-700  ml-3" />
